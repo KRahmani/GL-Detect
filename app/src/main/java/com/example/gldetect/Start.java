251a17988c5d7e694aeae14bf4a129f3c4be0063
kahina <kahinarahmani0@gmail.com>
2020-05-17 15:26:45 +0200

    implémentation modele DL

--- app/src/main/java/com/example/gldetect/Start.java
+++ app/src/main/java/com/example/gldetect/Start.java
@@ -1,21 +1,27 @@
 package com.example.gldetect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -25,229 +31,160 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import com.soundcloud.android.crop.Crop;
+
+import static com.soundcloud.android.crop.Crop.REQUEST_PICK;
 
 public class Start extends AppCompatActivity {
     private static ImageView home_img;
     private static LinearLayout cam_btn,lib_btn;
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
-    private static final int LIB_PERMISSION_REQUEST_CODE = 2;
-    private static final String FOLDER_NAME="GL Detect";
+    final Context context = this;
+
+    // for permission requests
+    public static final int REQUEST_PERMISSION = 300;
+
+    // request code for permission requests to the os for image
+    public static final int REQUEST_IMAGE = 100;
+    public static final int PICK_IMAGE_REQUEST = 200;
+
+    // will hold uri of image obtained from camera
+    private Uri imageUri;
+
+    // string to send to next activity that describes the chosen classifier
+    private String nomModele;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // request permission to use the camera on the user's phone
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+        }
+
+        // request permission to write data (aka images) to the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
+        // request permission to read data (aka images) from the user's external storage of their phone
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+
         home_img= (ImageView) findViewById(R.id.home_icn);
         cam_btn=(LinearLayout)findViewById(R.id.cam_btn);
         lib_btn=(LinearLayout)findViewById(R.id.lib_btn);
-        final Context context = this;
+        nomModele = "model.tflite";
+
         home_img.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(context,MainActivity.class);
                 startActivity(intent);
             }
         });
+
         cam_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(context,
-                        Manifest.permission.CAMERA)
-                        != PackageManager.PERMISSION_GRANTED ||
-                        ContextCompat.checkSelfPermission(context,
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                != PackageManager.PERMISSION_GRANTED)
-                {
-                    // Permission is not granted
-                    //request permission for camera and external storage writing
-                    ActivityCompat.requestPermissions(Start.this,
-                            new String[]{Manifest.permission.CAMERA,
-                                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                             CAMERA_PERMISSION_REQUEST_CODE);
-                }
-                else openCamera();
+                // open camera
+                openCameraIntent();
+
             }
         });
         lib_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(context,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED ||
-                        ContextCompat.checkSelfPermission(context,
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                                != PackageManager.PERMISSION_GRANTED)
-                {
-                    // Permission is not granted
-                    //request permission for library
-                    ActivityCompat.requestPermissions(Start.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-                                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            LIB_PERMISSION_REQUEST_CODE);
-                }
-                else
-                    openLibrary();
+                // open camera
+                openLibrary();
             }
         });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case CAMERA_PERMISSION_REQUEST_CODE: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    // permission was granted,
-                    openCamera();
-
-                else
-                    // permission denied
-                    if (ActivityCompat.shouldShowRequestPermissionRationale
-                            (this, Manifest.permission.CAMERA))
-                        showAlert(CAMERA_PERMISSION_REQUEST_CODE);
-                return;
-            }
-            case LIB_PERMISSION_REQUEST_CODE: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    // permission was granted,
-                    openLibrary();
-
-                else
-                    // permission denied
-                    if (ActivityCompat.shouldShowRequestPermissionRationale
-                            (this, Manifest.permission.READ_EXTERNAL_STORAGE))
-                        showAlert(LIB_PERMISSION_REQUEST_CODE);
-                return;
-            }
-        }
 
     }
 
-    private void openCamera() {
-        //createFolder();
-        createExternalStoragePublicPicture();
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
-
-
-        //intent = new Intent(this,ChoosePic.class);
-        //startActivity(intent);
+    // opens camera for user
+    private void openCameraIntent(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_IMAGE);
     }
+
     private void openLibrary(){
         //createFolder();
-        createExternalStoragePublicPicture();
+        //createExternalStoragePublicPicture();
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivity(intent);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
 
         //intent = new Intent(this,ChoosePic.class);
         //startActivity(intent);
     }
-
-    private void showAlert(final int permisionCode) {
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle(getString(R.string.warningTitle));
-        alertDialog.setMessage(getString(R.string.warning));
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.cancel),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        finish();
-                        Toast.makeText(Start.this,
-                                getString(R.string.denied),
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        switch(permisionCode) {
-                            case CAMERA_PERMISSION_REQUEST_CODE:
-                                ActivityCompat.requestPermissions(Start.this,
-                                        new String[]{Manifest.permission.CAMERA,
-                                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                        CAMERA_PERMISSION_REQUEST_CODE);
-                                break;
-                            case LIB_PERMISSION_REQUEST_CODE:
-                                ActivityCompat.requestPermissions(Start.this,
-                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-                                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                        LIB_PERMISSION_REQUEST_CODE);
-                                break;
-                        }
-
-                    }
-                });
-        alertDialog.show();
-    }
-
-    public void createFolder(){
-
-        File f= new File(Environment.getExternalStoragePublicDirectory
-                (Environment.DIRECTORY_PICTURES)+"/"+FOLDER_NAME);
-        boolean exists=f.exists();
-
-        if(!exists){
-            Toast.makeText(this, " doesn't exists", Toast.LENGTH_SHORT).show();
-            f.mkdirs();
-            f.setExecutable(true);
-            f.setReadable(true);
-            f.setWritable(true);
-        }
-
-        else
-            Toast.makeText(this, " already exits.", Toast.LENGTH_SHORT).show();
-
-    }
-
-    void createExternalStoragePublicPicture() {
-        // Create a path where we will place our picture in the user's
-        // public pictures directory.  Note that you should be careful about
-        // what you place here, since the user often manages these files.  For
-        // pictures and other media owned by the application, consider
-        // Context.getExternalMediaDir().
-        File path = new File(Environment.getExternalStorageDirectory()+"/"+FOLDER_NAME);
-        File file = new File(path, "exemplePicture.jpg");
-        if (file.exists()) return;
-
-        try {
-            // Make sure the Pictures directory exists.
-            path.mkdirs();
-
-            // Very simple code to copy a picture from the application's
-            // resource into the external file.  Note that this code does
-            // no error checking, and assumes the picture is small (does not
-            // try to copy it in chunks).  Note that if external storage is
-            // not currently mounted this will silently fail.
-            InputStream is = getResources().openRawResource(R.raw.exemple);
-            OutputStream os = new FileOutputStream(file);
-            byte[] data = new byte[is.available()];
-            is.read(data);
-            os.write(data);
-            is.close();
-            os.close();
-
-            // Tell the media scanner about the new file so that it is
-            // immediately available to the user.
-            MediaScannerConnection.scanFile(this,
-                    new String[] { file.toString() }, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                            Log.i("ExternalStorage", "Scanned " + path + ":");
-                            Log.i("ExternalStorage", "-> uri=" + uri);
-                        }
-                    });
-        } catch (IOException e) {
-            // Unable to create file, likely because external storage is
-            // not currently mounted.
-            Log.w("ExternalStorage", "Error writing " + file, e);
+    // checks that the user has allowed all the required permission of read and write and camera. If not, notify the user and close the application
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(),"This application needs read, write, and camera permissions to run. Application now closing.",Toast.LENGTH_LONG);
+                System.exit(0);
+            }
         }
     }
 
+    // dictates what to do after the user takes an image, selects and image, or crops an image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'LancerDiangnostic' activity
+        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            try {
+                Uri source_uri = imageUri;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(Start.this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // if the library activity is finished, obtained the uri, crop it to make it square, and send it to 'LancerDiagnostic' activity
+        else if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK ){
+            try {
+                Uri source_uri = data.getData();;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(Start.this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // if cropping acitivty is finished, get the resulting cropped image uri and send it to 'Classify' activity
+        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK ){
+            imageUri = Crop.getOutput(data);
+            Intent i = new Intent(context, LancerDiagnostic.class);
+            // put image data in extras to send
+            i.putExtra("resID_uri", imageUri);
+            // put filename in extras
+            i.putExtra("chosen", nomModele);
+            // send other required data
+            startActivity(i);
+        }
+
+    }
 }
 
